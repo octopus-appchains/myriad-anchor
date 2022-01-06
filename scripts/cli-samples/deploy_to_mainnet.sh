ee775cd11dc608a3700dd2c14d02989f7ed8fe25
Rivers Yang <rivers.yang@icloud.com>
2022-01-06 22:23:13 +0800

    Correct crate version, initial settings and deployment scripts.

--- scripts/cli-samples/deploy_to_mainnet.sh
+++ scripts/cli-samples/deploy_to_mainnet.sh
@@ -1,14 +1,17 @@
 #
 export NEAR_ENV=mainnet
 #
+export ANCHOR_ACCOUNT_ID=myriad.octopus-registry.near
+export OWNER_ACCOUNT_ID=octopus-registry.near
 #
 #
-near deploy --accountId debionetwork.octopus-registry.near --wasmFile res/appchain_anchor.wasm
-near call debionetwork.octopus-registry.near new '{"appchain_id":"debionetwork","appchain_registry":"octopus-registry.near","oct_token":"f5cfbc74057c610c8ef151a439252680ac68c6dc.factory.bridge.near"}' --accountId octopus-registry.near --gas 200000000000000
-near call f5cfbc74057c610c8ef151a439252680ac68c6dc.factory.bridge.near storage_deposit '{"account_id":"debionetwork.octopus-registry.near","registration_only":null}' --accountId octopus-registry.near --deposit 1
+near deploy --accountId $ANCHOR_ACCOUNT_ID --wasmFile res/appchain_anchor.wasm
+near call $ANCHOR_ACCOUNT_ID new '{"appchain_id":"myriad","appchain_registry":"octopus-registry.near","oct_token":"f5cfbc74057c610c8ef151a439252680ac68c6dc.factory.bridge.near"}' --accountId $OWNER_ACCOUNT_ID --gas 200000000000000
+near call f5cfbc74057c610c8ef151a439252680ac68c6dc.factory.bridge.near storage_deposit '{"account_id":"myriad.octopus-registry.near","registration_only":null}' --accountId $OWNER_ACCOUNT_ID --deposit 0.0125
 #
-near call debionetwork.octopus-registry.near migrate_state '' --accountId octopus-registry.near --gas 200000000000000
+near call $ANCHOR_ACCOUNT_ID set_token_price_maintainer_account '{"account_id":"octopus-registry.near"}' --accountId $OWNER_ACCOUNT_ID
+near call $ANCHOR_ACCOUNT_ID set_price_of_oct_token '{"price":"4000000"}' --accountId $OWNER_ACCOUNT_ID
 #
-near call debionetwork.octopus-registry.near set_token_price_maintainer_account '{"account_id":"octopus-registry.near"}' --accountId octopus-registry.near
-near call debionetwork.octopus-registry.near set_price_of_oct_token '{"price":"2740000"}' --accountId octopus-registry.near
+near call $ANCHOR_ACCOUNT_ID set_relayer_account '{"account_id":"octopus-counter.near"}' --accountId $OWNER_ACCOUNT_ID
 #
+near call $ANCHOR_ACCOUNT_ID migrate_state '' --accountId $OWNER_ACCOUNT_ID --gas 200000000000000
